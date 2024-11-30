214bfa086837efc84f04a10132bb979b142688df
emrekardaslar <emre.kardaslar@gmail.com>
2024-12-01 00:15:08 +0300

    add image and update message card

--- app/src/main/java/emre/workspace/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/emre/workspace/jetpackcomposedemo/MainActivity.kt
@@ -3,28 +3,43 @@ package emre.workspace.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                MessageCard("Emre")
+                MessageCard(Message("Android", "Jetpack Compose"))
             }
         }
-
 }
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(name: String) {
-    Text("Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.ppgithub),
+            contentDescription = "Contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
-
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android Phone")
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
