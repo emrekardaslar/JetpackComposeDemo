7a8c8f31a3ad516de61f185b7d13eb892e7b1ec6
emrekardaslar <emre.kardaslar@gmail.com>
2024-12-01 00:05:31 +0300

    init project, add a preview

---
+++ app/src/main/java/emre/workspace/jetpackcomposedemo/MainActivity.kt
@@ -0,0 +1,30 @@
+package emre.workspace.jetpackcomposedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                MessageCard("Emre")
+            }
+        }
+
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text("Hello $name!")
+}
+
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android Phone")
+}
