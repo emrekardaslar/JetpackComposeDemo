bcdbffea2ca842291e3efa7bb7ffddf7daf795c6
emrekardaslar <emre.kardaslar@gmail.com>
2025-03-08 14:56:18 +0300

    pass background color to Home Screen

--- app/src/main/java/emre/workspace/jetpackcomposedemo/ui/screens/HomeScreen.kt
+++ app/src/main/java/emre/workspace/jetpackcomposedemo/ui/screens/HomeScreen.kt
@@ -1,19 +1,27 @@
 package emre.workspace.jetpackcomposedemo.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import emre.workspace.jetpackcomposedemo.data.model.Message
 import emre.workspace.jetpackcomposedemo.ui.components.Conversation
 
 @Composable
 fun HomeScreen(
     messages: List<Message>,
-    onMessageClick: (Message) -> Unit // Callback for navigating to the detail screen
+    onMessageClick: (Message) -> Unit,
+    backgroundColor: String
 ) {
     Scaffold { padding ->
-        // Use Conversation instead of LazyColumn
+        androidx.compose.material3.Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(android.graphics.Color.parseColor(backgroundColor))
+        ) {
+
+        }
         Conversation(
             messages = messages,
             onMessageClick = onMessageClick,
